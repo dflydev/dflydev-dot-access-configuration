47aa497e300756a70e88423aadfda701e2fd11c1
Beau Simensen <beau@dflydev.com>
2012-07-17 13:25:10 -0700

    Account for Composer changes.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,5 @@
  * file that was distributed with this source code.
  */
 
-$loader = require dirname(__DIR__).'/vendor/.composer/autoload.php';
+$loader = require dirname(__DIR__).'/vendor/autoload.php';
 $loader->add('Dflydev\\Tests\\DotAccessConfiguration', 'tests');
